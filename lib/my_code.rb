dcbdd33ccc80cf0d90032bb7debe80b6eab2bb1a
Matt Deason <d.sonmd@gmail.com>
2019-07-18 00:56:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# My Code here....
+def map_to_negativize (array)
+  array.map do | number |
+    number * -1
+  end
+
+end
+
+def map_to_no_change (array)
+  array.map do | name |
+    name
+  end
+end
+
+def map_to_double (array)
+  array.map do | number |
+    number * 2 
+  end
+end
+
+def map_to_square (array)
+  array.map do | number |
+    number ** 2 
+  end
+end
+
+def reduce_to_total
+  
